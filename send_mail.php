6b56841ec7c2b22b217c8a1696a38eaaa35346ee
unknown <vembisaputra273@gmail.com>
2024-08-11 07:42:15 +0700

    new post

---
+++ send_mail.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "vembisaputra273@gmail.com"; 
+    $subject = "Pesan Baru dari Form Kontak";
+    $body = "Nama: $name\nEmail: $email\n\nPesan:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Pesan berhasil dikirim!";
+    } else {
+        echo "Pesan gagal dikirim.";
+    }
+}
+?>
